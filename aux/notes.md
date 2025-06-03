c2d98b2267425447de89458545a2d65b01690c1c
ash <anaa.ceban@gmail.com>
2025-06-03 11:08:28 +0200

    3 config files

---
+++ aux/notes.md
@@ -0,0 +1,15 @@
+Each problem's instance require three datafiles :
+
+1. constraint description file
+2. permeability information data
+3. economic informations data
+
+ex:
+/005 Arkema_1.dat
+/038 Arkema_1_eco.dat
+/086 Arkema_1_fix_perm.dat
+
+
+Arkema_1.data
+
+
--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-from PyQt6.QtWidgets import QMainWindow, QLabel, QVBoxLayout, QWidget
+from PyQt6.QtWidgets import (
+    QFormLayout,
+    QLineEdit,
+    QMainWindow,
+    QLabel,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
 import sys
 from PyQt6.QtWidgets import QApplication
 
@@ -10,15 +18,43 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("App")
         self.setMinimumSize(600, 400)
 
-        label = QLabel("Bonjour ?")
-
-        layout = QVBoxLayout()
-        layout.addWidget(label)
-
         central_widget = QWidget()
-        central_widget.setLayout(layout)
+        form_layout = QFormLayout()
+        self.param1_input = QLineEdit()
+        self.param2_input = QLineEdit()
+        self.param3_input = QLineEdit()
+        form_layout.addRow("Parameter 1:", self.param1_input)
+        form_layout.addRow("Parameter 2:", self.param2_input)
+        form_layout.addRow("Parameter 3:", self.param3_input)
+        central_widget.setLayout(form_layout)
+        self.submit_button = QPushButton("Submit", self)
+        self.submit_button.clicked.connect(self.generate_file)
+        self.submit_button.clicked.connect(self.generate_file_eco)
+        self.submit_button.clicked.connect(self.generate_file_perm)
+        form_layout.addWidget(self.submit_button)
 
         self.setCentralWidget(central_widget)
+
+    def generate_file(self):
+        param1 = self.param1_input.text()
+
+        with open("data/file_eco.dap", "w") as f:
+            f.write(f"param pressure_in := {param1};\n")
+        print("the file file_eco.dap was generated successfully")
+
+    def generate_file_eco(self):
+        param2 = self.param2_input.text()
+
+        with open("data/file_perm.dap", "w") as f:
+            f.write(f"param pressure_in := {param2};\n")
+        print("the file file_perm.dap was generated successfully")
+
+    def generate_file_perm(self):
+        param3 = self.param3_input.text()
+
+        with open("data/file.dap", "w") as f:
+            f.write(f"param pressure_in := {param3};\n")
+        print("the file file.dap was generated successfully")
 
 
 if __name__ == "__main__":
